9d1fd2bbe145b27925216129f640ee23b3824e96
MarcelaLopez3 <37322208+MarcelaLopez3@users.noreply.github.com>
2018-09-28 11:36:45 -0600

    Agregando formulario de personas juridicas y masterpage

---
+++ ProyectoASPEmenic/ProyectoASPEmenic/Paginas/Clientes/RegistroJuridicos.aspx
@@ -0,0 +1,122 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="RegistroJuridicos.aspx.cs" Inherits="ProyectoASPEmenic.Paginas.Clientes.RegistroJuridicos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <center><h1><asp:Label runat="server" ID="lblpersonanatural" Text="Persona Jurídica" style="color:black; font-family:'Times New Roman', Times, serif"></asp:Label></h1></center>
+    <br />
+    <div class="card border-primary mb-3">
+        <div class="card-header"><h4>Datos Generales</h4></div>
+        <div class="card-body text-primary">
+            <div class="form-row">
+                <div class="form-group col-md-3">
+                    <asp:Label runat="server" ID="lblrazonsocial" Text="Razón Social:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtrazonsocial" CssClass="form-control"></asp:TextBox>    
+                </div>
+                <div class="form-group col-md-3">    
+                    <asp:Label runat="server" ID="lblnombrecomercial" Text="Nombre Comercial:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtnombrecomercial" CssClass="form-control" /> 
+               </div>
+                <div class="form-group col-md-3">
+                    <asp:Label runat="server" ID="lblgiroactividadeconomica" Text="Giro/Actividad Economica:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtgiroactividadeconomica" CssClass="form-control"></asp:TextBox>    
+                </div>
+                <div class="form-group col-md-3">    
+                    <asp:Label runat="server" ID="lbltamanoempresa" Text="Tamaño Empresa:"  style="color:black"></asp:Label>
+                    <asp:DropDownList ID="ddltamanoempresa" runat="server" CssClass="form-control" class="form-control">
+                    <asp:ListItem Text="Pequeña" Value="Pequeña"></asp:ListItem>
+                    <asp:ListItem Text="Mediana" Value="Mediana"></asp:ListItem>
+                    <asp:ListItem Text="Grande" Value="Grande"></asp:ListItem>
+                </asp:DropDownList> 
+               </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="lbldireccionubicacion" Text="Dirección/Ubicación:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtdireccionubicacion" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label2" Text="Municipio/Ciudad:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtmunicipiociudad" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label1" Text="Departamento/Estado:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtdepartamentoestado" CssClass="form-control"></asp:TextBox> 
+            </div>
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label3" Text="Pais:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtpais" CssClass="form-control"></asp:TextBox>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label12" Text="Telefono:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txttelefono" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label13" Text="Fax:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtFax" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label14" Text="Correo electronico:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtcorreoelectronico" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group col-md-3">
+                <asp:Label runat="server" ID="Label4" Text="Codigo Postal:" style="color:black"></asp:Label>
+                <asp:TextBox runat="server" ID="txtcodifopostal" CssClass="form-control"></asp:TextBox>
+            </div>
+        </div>
+        </div>
+    </div>
+    <br />
+    <div class="card border-primary mb-3">
+        <div class="card-header"><h4>Datos Identificación</h4></div>
+        <div class="card-body text-primary">
+            <div class="form-row">
+                <div class="form-group col-md-3">
+                    <asp:Label runat="server" ID="Label5" Text="NIT:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtNIT" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group col-md-3">
+                    <asp:Label runat="server" ID="Label6" Text="Fecha expedición NIT:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtfechaexpedicionNIT" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group col-md-6">
+                    <asp:Label runat="server" ID="Label7" Text="Nombre según NIT:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtnombreNIT" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-3">
+                    <asp:Label runat="server" ID="Label8" Text="NRC:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtNRC" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group col-md-3">
+                    <asp:Label runat="server" ID="Label9" Text="Fecha expedición NRC:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtfechaexpedicónNRC" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group col-md-6">
+                    <asp:Label runat="server" ID="Label10" Text="Nombre según NRC:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtnombreNRC" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br />
+    <div class="card border-primary mb-3">
+        <div class="card-header"><h4>Otros</h4></div>
+        <div class="card-body text-primary">
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <asp:Label runat="server" ID="Label15" Text="Representante Legal:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtrepresentantelegal" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <asp:Label runat="server" ID="Label11" Text="Observaciones:" style="color:black"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtobservaciones" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ ProyectoASPEmenic/ProyectoASPEmenic/Paginas/Clientes/RegistroJuridicos.aspx.designer.cs
@@ -0,0 +1,384 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoASPEmenic.Paginas.Clientes {
+    
+    
+    public partial class RegistroJuridicos {
+        
+        /// <summary>
+        /// Control lblpersonanatural.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblpersonanatural;
+        
+        /// <summary>
+        /// Control lblrazonsocial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblrazonsocial;
+        
+        /// <summary>
+        /// Control txtrazonsocial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtrazonsocial;
+        
+        /// <summary>
+        /// Control lblnombrecomercial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblnombrecomercial;
+        
+        /// <summary>
+        /// Control txtnombrecomercial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombrecomercial;
+        
+        /// <summary>
+        /// Control lblgiroactividadeconomica.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblgiroactividadeconomica;
+        
+        /// <summary>
+        /// Control txtgiroactividadeconomica.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtgiroactividadeconomica;
+        
+        /// <summary>
+        /// Control lbltamanoempresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbltamanoempresa;
+        
+        /// <summary>
+        /// Control ddltamanoempresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddltamanoempresa;
+        
+        /// <summary>
+        /// Control lbldireccionubicacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbldireccionubicacion;
+        
+        /// <summary>
+        /// Control txtdireccionubicacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtdireccionubicacion;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control txtmunicipiociudad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtmunicipiociudad;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control txtdepartamentoestado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtdepartamentoestado;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control txtpais.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtpais;
+        
+        /// <summary>
+        /// Control Label12.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+        
+        /// <summary>
+        /// Control txttelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txttelefono;
+        
+        /// <summary>
+        /// Control Label13.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+        
+        /// <summary>
+        /// Control txtFax.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFax;
+        
+        /// <summary>
+        /// Control Label14.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label14;
+        
+        /// <summary>
+        /// Control txtcorreoelectronico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtcorreoelectronico;
+        
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Control txtcodifopostal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtcodifopostal;
+        
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Control txtNIT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNIT;
+        
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Control txtfechaexpedicionNIT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfechaexpedicionNIT;
+        
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Control txtnombreNIT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombreNIT;
+        
+        /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// Control txtNRC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNRC;
+        
+        /// <summary>
+        /// Control Label9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+        
+        /// <summary>
+        /// Control txtfechaexpedicónNRC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfechaexpedicónNRC;
+        
+        /// <summary>
+        /// Control Label10.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+        
+        /// <summary>
+        /// Control txtnombreNRC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombreNRC;
+        
+        /// <summary>
+        /// Control Label15.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+        
+        /// <summary>
+        /// Control txtrepresentantelegal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtrepresentantelegal;
+        
+        /// <summary>
+        /// Control Label11.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+        
+        /// <summary>
+        /// Control txtobservaciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtobservaciones;
+    }
+}
