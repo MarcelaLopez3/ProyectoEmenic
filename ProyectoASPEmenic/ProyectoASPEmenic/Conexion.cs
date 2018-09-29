1ca616d31d60f9aabbc400e8c6d1ddf91af2d0de
MarcelaLopez3 <37322208+MarcelaLopez3@users.noreply.github.com>
2018-09-29 03:05:36 -0600

    Cambios Formularios, Clase conexion, y insert de Naturales

---
+++ ProyectoASPEmenic/ProyectoASPEmenic/Conexion.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace ProyectoASPEmenic
+{
+    public class Conexion
+    {
+        public Conexion()
+        {
+
+        }
+
+        //variables de conexion
+        public MySqlConnection cnn = new MySqlConnection();
+        //MySqlConnection cnn = new MySqlConnection("server=localhost:81;database=bdd_emenic;userid=root2;password=123456;charsetutf8;");
+        public MySqlDataReader reg;
+        
+
+        //funcion que inicia conexion
+        public void IniciarConexion()
+        {
+            try
+            {
+                cnn.ConnectionString = (@"Server=localhost;Port=81;Database=bdd_emenic;Uid=root2;Pwd=123456;charsetutf8;");
+                cnn.Open();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            //using (cnn)
+            //{
+            //    cnn.Open();
+            //}
+
+        }
+
+        public void CerrarConexion()
+        {
+            cnn.Close();
+        }
+
+        public void EnviarQuery(string query)
+        {
+            MySqlCommand con = new MySqlCommand(query, cnn);
+            con.ExecuteNonQuery();
+        }
+
+        public MySqlDataReader RecibeQuery(string query)
+        {
+            MySqlCommand con = new MySqlCommand(query, cnn);
+            con.ExecuteNonQuery();
+            reg = con.ExecuteReader();
+            return reg;
+        }
+
+        public void TablasQuery(string query, GridView tabla)
+        {
+            try
+            {
+                MySqlCommand con = new MySqlCommand(query, cnn);
+                MySqlDataAdapter da = new MySqlDataAdapter(con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                tabla.DataSource = dt;
+                tabla.DataBind();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ProyectoASPEmenic/ProyectoASPEmenic/Paginas/Personas/RegistroNaturales.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoASPEmenic.Paginas.Clientes
+{
+    public partial class RegistroNaturales : System.Web.UI.Page
+    {
+        Conexion conexion = new Conexion();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGuardarNaturales_Click(object sender, EventArgs e)
+        {
+            //recuperando entradas
+            string PrimerNombre = txtprimernombre.Text;
+            string SegundoNombre = txtsegundonombre.Text;
+            string PrimerApellido = txtprimerapellido.Text;
+            string SegundoApellido = txtsegundoapellido.Text;
+            int Edad = Int32.Parse(txtedad.Text);
+            string Genero = ddlgenero.SelectedValue;
+            string EstadoCivil = ddlestadocivil.SelectedValue;
+            string ProfesionOficio = txtprofesionoficio.Text;
+            string DireccionResidencia = txtdireccionresidencia.Text;
+            string DepartamentoResidencia = txtdepartamentoresidencia.Text;
+            string MunicipioResidencia = txtmunicipioresidencia.Text;
+            DateTime FechaNacimiento = DateTime.Parse(txtfechanacimiento.Text);
+            string DepartamentoNacimiento = txtdepartamentonacimiento.Text;
+            string MunicipioNacimiento = txtmunicipionacimiento.Text;
+            string DUI = txtDUI.Text;
+            string NIT = txtNIT.Text;
+            string NumeroLicencia = txtnumerolicencia.Text;
+            string Pasaporte = txtpasaporte.Text;
+            string NombreOtro = txtnombreotro.Text;
+            string NumeroOtro = txtnumerootro.Text;
+            string Telefono1 = txttelefono1.Text;
+            string Telefono2 = txttelefono2.Text;
+            string Telefono3 = txttelefono3.Text;
+            string Celular = txtcelular.Text;
+            string Email1 = txtemail1.Text;
+            string Email2 = txtemail2.Text;
+            string Observaciones = txtobservaciones.Text;
+            Boolean Usuario = checkUsuario.Checked;
+            Boolean Empleado = checkEmpleado.Checked;
+            Boolean Cliente = checkCliente.Checked;
+            Boolean RepresentanteLegal = checkRepresentanteLegal.Checked;
+            Boolean Proveedor = checkProveedor.Checked;
+            Boolean Socio = checkSocio.Checked;
+            Boolean PersonaNatural = true;
+
+            //consulta que se ingresa a la base de datos
+            string query = "INSERT INTO persona(PrimerNombre,SegundoNombre,PrimerApellido,SegundoApellido,Edad," +
+                "Genero,EstadoCivil,ProfesionOficio,DireccionResidencia,DepartamentoResidencia,MunicipioResidencia," +
+                "FechaNacimiento,DepartamentoNacimiento,MunicipioNacimiento,DUI,NIT,NumeroLicencia,Pasaporte,NombreOtro," +
+                "NumeroOtro,Telefono1,Telefono2,Telefono3,Celular,Email1,Email2,Observaciones,Usuario,Empleado,Cliente," +
+                "RepresentanteLegal,Proveedor,Socio,PersonaNatural) VALUES ('" + PrimerNombre +"','" + SegundoNombre + "','" +
+                PrimerApellido + "','" + SegundoApellido + "'," + Edad +",'" + Genero + "','" + EstadoCivil + "','" + ProfesionOficio + 
+                "','" + DireccionResidencia + "','" + DepartamentoResidencia + "','" + MunicipioResidencia + "','" + FechaNacimiento + 
+                "','" +  DepartamentoNacimiento + "','" + MunicipioNacimiento + "','" + DUI + "','" + NIT + "','" + NumeroLicencia + 
+                "','" + Pasaporte + "','" + NombreOtro + "','" + NumeroOtro + "','" + Telefono1 + "','" + Telefono2 + "','" + Telefono3 +
+                "','" + Celular + "','" + Email1 + "','" + Email2 + "','" + Observaciones + "'," + Usuario + "," + Empleado + 
+                "," + Cliente + "," + RepresentanteLegal + "," + Proveedor + "," + Socio + "," + PersonaNatural +")";
+
+            //enviar consulta a Mysql
+            conexion.IniciarConexion();
+            conexion.EnviarQuery(query);
+            conexion.CerrarConexion();
+
+        }
+    }
+}
