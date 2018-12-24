b49de968f50598e7b76f74f241db4759db1246e0
SusanQuintanilla16 <susan.quintanilla@gmail.com>
2018-12-23 22:17:32 -0600

    Cargar datos en tarjetas dashboard

--- ProyectoASPEmenic/ProyectoASPEmenic/Paginas/Dashboard.aspx.cs
+++ ProyectoASPEmenic/ProyectoASPEmenic/Paginas/Dashboard.aspx.cs
@@ -9,9 +9,48 @@ namespace ProyectoASPEmenic.Paginas
 {
     public partial class Dashboard : System.Web.UI.Page
     {
+        Conexion cn = new Conexion();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                loadCards();
+            }
+        }
 
+        //Función para cargar datos de las tarjetas
+        private void loadCards()
+        {
+            string query = "SELECT count(*) as TotalClientes from persona where persona.Cliente=1";
+            cn.IniciarConexion();
+            cn.RecibeQuery(query);
+            while (cn.reg.Read())
+            {
+                lblClientes.Text = cn.reg.GetString(0);
+            }
+            cn.reg.Close();
+            query = "SELECT count(*) as TotalEquipo from transporte";
+            cn.RecibeQuery(query);
+            while (cn.reg.Read())
+            {
+                lblEquipo.Text = cn.reg.GetString(0);
+            }
+            cn.reg.Close();
+            query = "SELECT count(*) from serviciocontratado";
+            cn.RecibeQuery(query);
+            while (cn.reg.Read())
+            {
+                lblServicios.Text = cn.reg.GetString(0);
+            }
+            cn.reg.Close();
+            query = "SELECT count(*) from usuario WHERE usuario.Estado=1";
+            cn.RecibeQuery(query);
+            while (cn.reg.Read())
+            {
+                lblUsuarios.Text = cn.reg.GetString(0);
+            }
+            cn.CerrarConexion();
         }
     }
 }
